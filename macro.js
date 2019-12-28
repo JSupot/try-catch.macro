1851f1c6decf3b57a7cdd900d62747b9b04dde6d
JSupot <huchisupot@163.com>
2019-12-28 12:01:13 +0800

    first version

--- macro.js
+++ macro.js
@@ -1,7 +1,37 @@
 const { createMacro } = require('babel-plugin-macros');
 
 function macro({ babel, references, state }) {
+  const { types, template } = babel;
+  const wrapFunctionWithTryCatch = template(`{
+    try {
+      BODY
+    } catch(e) {
+      console.error(e);
+    }
+  }`);
 
+  references.TryCatch.forEach(reference => {
+    const parentFunction = reference.getFunctionParent();
+
+    if (!parentFunction) {
+      return;
+    }
+
+    const body = parentFunction.node.body.body;
+
+    if (!body || !body.length) {
+      return;
+    }
+
+    if (body.length === 1 && types.isTryStatement(body[0])) {
+      return;
+    }
+
+    parentFunction.get('body').replaceWith(wrapFunctionWithTryCatch({
+      BODY: body,
+    }));
+
+  });
 }
 
 module.exports = createMacro(macro)
